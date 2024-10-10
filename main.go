5d149eb7c0d715132c144348dd6d5a32564f5152
Shashank Pachava <spachava753@gmail.com>
2024-10-10 14:32:32 -0400

    Update application version to 0.11.4
    This change updates the version constant in main.go from 0.11.2 to 0.11.4

--- main.go
+++ main.go
@@ -143,7 +143,7 @@ func printTokenUsage(usage llm.TokenUsage) {
 	fmt.Printf("-------------------\n")
 }
 
-const version = "0.11.2"
+const version = "0.11.4"
 
 func main() {
 	flags := ParseFlags()
